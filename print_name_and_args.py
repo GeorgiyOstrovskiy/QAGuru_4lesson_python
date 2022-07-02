87a8c5e962bad295f3e8d210df34ce7eed05239b
xacker <egor_kinderr@mail.ru>
2022-07-03 02:17:01 +0300

    change func, working nice

--- print_name_and_args.py
+++ print_name_and_args.py
@@ -1,30 +1,12 @@
 import inspect
 
 
-def open_browser():
-    pass
-
-
-def go_to_companyname_homepage(page_url):
-    pass
-
-
-def find_registration_button_on_login_page(page_url, button_text):
-    pass
-
-
 def print_name_and_args(func):
     name_func = func.__name__.split('_')
     args_func = inspect.getfullargspec(func).args
-    print('Name function:', end=' ')
-    for i in name_func:
-        print(i, end=' ')
+    print('Name function: ', *name_func)
     print('\nNames arguments:', end=' ')
     if not len(args_func):
         print('no arguments')
     else:
-        for i in args_func:
-            print(i, end=', ')
-
-
-print_name_and_args(find_registration_button_on_login_page)
+        print(*args_func, sep=', ')
